eaf0bbac330fa89d3d1855d3bb224985bcc9e503
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-25 04:21:52 -0500

    created an instance of new doctor with setDoctor on form.js- need to pass state from app.js to form.js

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -8,13 +8,27 @@ function Form() {
 	});
 	const onFormSubmit = event => {
 		event.preventDefault();
+
+		const newDoctor = {
+			fname: formValues.fname,
+			lname: formValues.lname,
+			specialty: formValues.specialty
+		};
+
+		setDoctor({ ...doctor, newDoctor });
 	};
 
 	const onInputChange = event => {
 		const changedInput = event.target.name;
-		const changedValue = event.target.value;
-		console.log(changedValue);
+		const newChangedValue = event.target.value;
+		console.log(newChangedValue);
+
+		setFormValues({
+			...formValues,
+			[changedInput]: newChangedValue
+		});
 	};
+
 	return (
 		<form onSubmit={onFormSubmit}>
 			<label>
