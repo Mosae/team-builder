b178895e180b3a1c29322e6ede1093fe1a00d995
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-25 02:20:33 -0500

    imported useState hook

---
+++ team-builder/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">Medical Team</header>
+		</div>
+	);
+}
+
+export default App;
