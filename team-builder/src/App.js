05ef7c862ab792c1d6f380549b8ac014f6604abf
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-25 02:37:40 -0500

    rendered the list of team memebers

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
-
 import './App.css';
 
+const listOfDoctors = [
+	{ fname: 'Nancy', lname: 'Jones' },
+	{ fname: 'Kelly', lname: 'Clarke' }
+];
+
 function App() {
+	const [doctor, setDoctor] = useState(listOfDoctors);
+
 	return (
 		<div className="App">
-			<header className="App-header">Medical Team</header>
+			<header className="App-header">
+				Medical Team
+				{doctor.map(dr => (
+					<div>
+						{dr.fname} {dr.lname}
+					</div>
+				))}
+			</header>
 		</div>
 	);
 }
